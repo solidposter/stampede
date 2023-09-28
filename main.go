add8810b9e844b5542e6933831595441eea42bc7
Tony Sarendal <tony@polarcap.org>
2023-09-28 07:11:17 +0200

    More doodling.

--- main.go
+++ main.go
@@ -7,19 +7,28 @@ import (
 
 func main() {
 
-	s := newServer("hemlignyckel", "9001")
-	go s.start()
+	serverconfig := message{
+		Key:   "hemlignyckel",
+		Id:    0,
+		Lport: 10000,
+		Hport: 10999,
+	}
+	fmt.Println(serverconfig)
 
-	c := newClient("2000")
-	c.start("127.0.0.1:9001")
+	s := newServer("9001")
+	go s.start(serverconfig)
+
+	c := newClient("2001")
+	msg := c.test("127.0.0.1:9001", "hemlignyckel")
+	fmt.Println(msg)
 	time.Sleep(1 * time.Second)
 
-	c = newClient("3000")
+	c = newClient("2000")
 	c.start("127.0.0.1:9001")
 	time.Sleep(1 * time.Second)
 
 	c = newClient("4000")
-	msg := c.test("127.0.0.1:9001", "hemlignyckel")
+	msg = c.test("127.0.0.1:9001", "hemlignyckel")
 	fmt.Println(msg)
 	time.Sleep(1 * time.Second)
 }
--- server.go
+++ server.go
@@ -1,36 +1,50 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net"
 )
 
 type server struct {
-	key  string // secret key
 	port string // low port in range
 }
 
-func newServer(key, port string) *server {
+func newServer(port string) *server {
 	return &server{
-		key:  key,
 		port: port,
 	}
 }
 
-func (s *server) start() {
+func (s *server) start(config message) {
+	m := message{}
+	nbuf := make([]byte, 1500)
+
 	conn, err := net.ListenPacket("udp", ":"+s.port)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	nbuf := make([]byte, 1500)
 	for {
 		length, addr, err := conn.ReadFrom(nbuf)
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		fmt.Println("received packet", addr, length)
+
+		dec := gob.NewDecoder(bytes.NewBuffer(nbuf[:length]))
+		err = dec.Decode(&m)
+		if err != nil {
+			fmt.Println("decode error:", err) // do I care ?
+			continue
+		}
+		fmt.Println(m)
+		if m.Key != config.Key {
+			fmt.Println("Key mismatch")
+			continue
+		}
+		fmt.Println("key match")
 	}
 }
